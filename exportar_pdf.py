429bfe73c7e9d0f274ab7d965ef9e17121b3ac23
Nahuel-Silva <nahuel_11_arg@hotmail.com>
2023-05-03 18:52:54 -0300

    falta medicion

--- exportar_pdf.py
+++ exportar_pdf.py
@@ -18,20 +18,20 @@ class Export():
             # Convertir las imágenes de OpenCV a Pillow Image
             image = Image.fromarray(img)
             # Agregar la primera imagen
-            pdf_file.drawImage(ImageReader(image), 100, 600, width=3*inch, height=3*inch)
+            pdf_file.drawImage(ImageReader(image), 100, 550, width=3*inch, height=3*inch)
 
 
         for img in image2:
             image = Image.fromarray(img)
             # Agregar la segunda imagen
-            pdf_file.drawImage(ImageReader(image), 300, 600, width=3*inch, height=3*inch)
+            pdf_file.drawImage(ImageReader(image), 350, 550, width=3*inch, height=3*inch)
 
         dist_1 = f"De marcador 3 a 1 hay una distancia de {dist_izq}"
         dist_2 = f"De marcador 2 a 0 hay una distancia de {dist_der}"
         # Agregar el string_value
         pdf_file.drawString(100, 500, result)
-        pdf_file.drawString(100, 400, dist_1)
-        pdf_file.drawString(100, 300, dist_2)
+        pdf_file.drawString(100, 480, dist_1)
+        pdf_file.drawString(100, 460, dist_2)
 
         # Guardar el archivo PDF
         pdf_file.save()
